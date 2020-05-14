4fc931a68a0f362b8c6af5ad9138ba3d22a37d10
alexandrurdev <alexcloudstar@gmail.com>
2020-05-14 11:12:49 +0300

    Updates

---
+++ precache-manifest.be9beab16968ed3146e678995e3ba6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7997ba8c174bfe082fc5c65e2aca84",
+    "url": "/bored/index.html"
+  },
+  {
+    "revision": "cf51bf99eedcfef49140",
+    "url": "/bored/static/css/main.26c4e087.chunk.css"
+  },
+  {
+    "revision": "0b701209851284f4f1de",
+    "url": "/bored/static/js/2.f4392f6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bored/static/js/2.f4392f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf51bf99eedcfef49140",
+    "url": "/bored/static/js/main.ddede474.chunk.js"
+  },
+  {
+    "revision": "8198758224d3ba586640",
+    "url": "/bored/static/js/runtime-main.5d6c6c1a.js"
+  }
+]);
